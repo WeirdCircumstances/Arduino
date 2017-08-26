67359a1990cce805e638deda70d79343b8467569
Benjamin Beuster <beusterb@posteo.de>
2017-08-26 14:49:42 +0200

    Hydrometer in Betrieb

--- AnalogPot-Hydrometer/AnalogPot-Hydrometer.ino
+++ AnalogPot-Hydrometer/AnalogPot-Hydrometer.ino
@@ -3,40 +3,44 @@ int latchPin = 5;
 int clockPin = 6;
 int dataPin = 4;
 int outputEnablePin = 3;
- 
+int ledoff = 13;
+
 int leds = 0;
- 
-void setup() 
+
+void setup()
 {
   pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);  
+  pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
+  pinMode(ledoff, OUTPUT);
   Serial.begin(9600);
 }
- 
-void loop() 
+
+void loop()
 {
+  //digitalWrite(ledoff,LOW);
   setBrightness(10);
   int reading  = analogRead(potPin);
-  //Serial.println(reading);
-  delay(1000);
-  int numLEDSLit = (1023 -(reading)) / 127;  //1023 / 9 //Nummer hinter reading anpassen für bessere Darstellung
+  //Serial.println(reading); //seriellen Anschluss auslesen
+  //delay(1000);
+  delay(600000); // in ms - 1x pro 10 min messen
+  int numLEDSLit = (1023 - (reading)) / 128; //1023 / 9 //Nummer hinter reading anpassen für bessere Darstellung
   leds = 0;
   for (int i = 0; i < numLEDSLit; i++)
   {
-    bitSet(leds, i); 
+    bitSet(leds, i);
   }
   updateShiftRegister();
 }
- 
+
 void updateShiftRegister()
 {
-   digitalWrite(latchPin, LOW);
-   shiftOut(dataPin, clockPin, LSBFIRST, leds);
-   digitalWrite(latchPin, HIGH);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
 }
 
 void setBrightness(byte brightness) // 0 to 255
 {
-  analogWrite(outputEnablePin, 255-brightness);
+  analogWrite(outputEnablePin, 255 - brightness);
 }
